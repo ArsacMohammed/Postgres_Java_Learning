c7894b13fccedab19898a38b46e0894d3f8721e5
ArsacMohammed <arsacskasha@gmail.com>
2023-06-21 13:45:20 +0530

    // creating a table and inserting  multiple values;

--- Postgres_learning/src/PostgreSQLExample.java
+++ Postgres_learning/src/PostgreSQLExample.java
@@ -8,7 +8,7 @@ public class PostgreSQLExample {
         String url = "jdbc:postgresql://localhost:5432/employee";
         String username = "postgres";
         String password = "Arsac@07";
-
+// creating a table and inserting  multiple values;
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the PostgreSQL database!");
@@ -24,8 +24,21 @@ public class PostgreSQLExample {
             	+"country VARCHAR(50) NOT NULL"
             	+	")";
 
-            statement.executeUpdate(createTable);
+            statement.executeUpdate(createTable); //because table creation is already executed.
             System.out.println("Table Created Successfully");
+            String insert1 ="INSERT INTO person (first_name, last_name, gender,email, date_of_birth, country) values 							('Khalil', 'Yurtsev', 'Male','kyurtsev0@cnbc.com','2022-05-29','China')";
+            // Perform database operations here
+            statement.executeUpdate(insert1);
+            System.out.println("inserted row 1 successfully");
+            String insert2 ="INSERT INTO person (first_name, last_name, gender,email, date_of_birth, country) values"
+            		+ "('Arsac', 'Yurtsev', 'Male','kyurtsev0@cnbc.com','2022-05-29','China'),"
+            		+ "('Arsac', 'Yurtsev', 'Male','kyurtsev0@cnbc.com','2022-05-29','China'),"
+            		+"('Arsac', 'Yurtsev', 'Male','kyurtsev0@cnbc.com','2022-05-29','China')";
+//note that + symbol and ',' at the end of each value (because of syntax in postgres
+            // Perform database operations here
+            statement.executeUpdate(insert2);
+            System.out.println("inserted row 2 successfully");
+            statement.close();
 
             connection.close();
         } catch (SQLException e) {

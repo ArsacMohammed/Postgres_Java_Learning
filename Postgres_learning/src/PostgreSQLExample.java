43c0b36d8b64653cab653c0934ffe6a77ffde35c
ArsacMohammed <arsacskasha@gmail.com>
2023-06-21 10:30:59 +0530

    connected to postgres database using java

--- Postgres_learning/src/PostgreSQLExample.java
+++ Postgres_learning/src/PostgreSQLExample.java
@@ -1,9 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class PostgreSQLExample {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "Arsac@07";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the PostgreSQL database!");
 
-	}
+            // Perform database operations here
 
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Connection failed. Error: " + e.getMessage());
+        }
+    }
 }
+
+
+
+// connecting to database server using java program
+

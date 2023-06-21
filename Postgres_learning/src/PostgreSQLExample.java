1ff6efb8e59afc6b8a80c8e15c5628ce466a19ae
ArsacMohammed <arsacskasha@gmail.com>
2023-06-21 13:18:07 +0530

    created a table

--- Postgres_learning/src/PostgreSQLExample.java
+++ Postgres_learning/src/PostgreSQLExample.java
@@ -5,22 +5,28 @@ import java.sql.Statement;
 
 public class PostgreSQLExample {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String url = "jdbc:postgresql://localhost:5432/employee";
         String username = "postgres";
         String password = "Arsac@07";
 
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the PostgreSQL database!");
-            Statement statement =connection.createStatement();
+            Statement statement = connection.createStatement();
             
-            String createDatabaseSQL =  "CREATE DATABASE Employee";
-            
-            statement.executeUpdate(createDatabaseSQL);
-            System.out.println("database created succesfully");
+            String createTable = "CREATE TABLE person("
+            	+"id BIGSERIAL NOT NULL PRIMARY KEY,"
+            	+"first_name VARCHAR(50) NOT NULL ,"
+            	+"last_name VARCHAR(50) NOT NULL,"
+            	+"gender VARCHAR(50) NOT NULL,"
+            	+"email VARCHAR(150) NOT NULL,"
+            	+"date_of_birth DATE NOT NULL,"
+            	+"country VARCHAR(50) NOT NULL"
+            	+	")";
 
-            // Perform database operations here
-            statement.close();
+            statement.executeUpdate(createTable);
+            System.out.println("Table Created Successfully");
+
             connection.close();
         } catch (SQLException e) {
             System.out.println("Connection failed. Error: " + e.getMessage());
